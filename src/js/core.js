dc9cbbba7f30ab829da6c38ed54922e3e7e92bbe
Francesco <francesco.miccolis98@libero.it>
2022-09-26 22:02:36 +0200

    core: logging qrcode generation src link

--- src/js/core.js
+++ src/js/core.js
@@ -842,6 +842,7 @@ const photoBooth = (function () {
             .append($('<i>').addClass('fa fa-times'))
             .css('float', 'right')
             .appendTo(body);
+        photoboothTools.console.logDev('api/qrcode.php?filename=' + filename + '&link=' + (customQrlink ? customQrlink : ''));
         $('<img src="api/qrcode.php?filename=' + filename + '&link=' + (customQrlink ? customQrlink : '') + '" alt="qr code"/>')
             .on('load', function () {
                 $('<p>')
