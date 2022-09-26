9b861eccdc97e0373c2cf28400f7f2b7e94f8918
Francesco <francesco.miccolis98@libero.it>
2022-09-26 19:32:15 +0200

    core: use the link from FTP server as input for QR code generation

--- src/js/core.js
+++ src/js/core.js
@@ -787,7 +787,7 @@ const photoBooth = (function () {
                 } else if (photoStyle === PhotoStyle.CHROMA) {
                     api.renderChroma(data.file);
                 } else {
-                    api.renderPic(data.file, data.images);
+                    api.renderPic(data.file, data.images, data.qr_link);
                 }
             },
             error: (jqXHR, textStatus) => {
@@ -826,7 +826,7 @@ const photoBooth = (function () {
         api.resetTimeOut();
     };
 
-    api.renderPic = function (filename, files) {
+    api.renderPic = function (filename, files, customQrlink) {
         const qrHelpText = config.qr.custom_text
             ? config.qr.text
             : photoboothTools.getTranslation('qrHelp') + '</br><b>' + config.webserver.ssid + '</b>';
@@ -842,7 +842,7 @@ const photoBooth = (function () {
             .append($('<i>').addClass('fa fa-times'))
             .css('float', 'right')
             .appendTo(body);
-        $('<img src="api/qrcode.php?filename=' + filename + '" alt="qr code"/>')
+        $('<img src="api/qrcode.php?filename=' + filename + '&link=' + customQrlink + '" alt="qr code"/>')
             .on('load', function () {
                 $('<p>')
                     .css('max-width', this.width + 'px')
