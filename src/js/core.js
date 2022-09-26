09f983c306798dbaf35761a41776cad954f62bd3
Francesco <francesco.miccolis98@libero.it>
2022-09-26 22:37:49 +0200

    core/api: more testing

--- src/js/core.js
+++ src/js/core.js
@@ -832,7 +832,7 @@ const photoBooth = (function () {
             .append($('<i>').addClass('fa fa-times'))
             .css('float', 'right')
             .appendTo(body);
-        photoboothTools.console.logDev('api/qrcode.php?filename=' + filename + '&link=' + (customQrlink ? customQrlink : ''));
+        photoboothTools.console.log('api/qrcode.php?filename=' + filename + '&link=' + (customQrlink ? customQrlink : ''));
         $('<img src="api/qrcode.php?filename=' + filename + '&link=' + (customQrlink ? customQrlink : '') + '" alt="qr code"/>')
             .on('load', function () {
                 $('<p>')
