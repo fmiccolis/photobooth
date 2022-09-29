e89d881a46c137491777a1286e2e98d46a99c63b
Francesco <francesco.miccolis98@libero.it>
2022-09-29 09:04:03 +0200

    pending commit NOT PUSH

--- src/js/core.js
+++ src/js/core.js
@@ -832,7 +832,6 @@ const photoBooth = (function () {
             .append($('<i>').addClass('fa fa-times'))
             .css('float', 'right')
             .appendTo(body);
-        photoboothTools.console.log('api/qrcode.php?filename=' + filename + '&link=' + (customQrlink ? customQrlink : ''));
         $('<img src="api/qrcode.php?filename=' + filename + '&link=' + (customQrlink ? customQrlink : '') + '" alt="qr code"/>')
             .on('load', function () {
                 $('<p>')
