13138a37eeed528a967734cf5defbf7298b02cc9
Francesco <francesco.miccolis98@libero.it>
2022-10-02 11:17:29 +0200

    print: added generation of qrcode with ftp server
    Change-Id: Ie2b35264f31a42b21f8b8c67bad3b32caea59910

--- api/print.php
+++ api/print.php
@@ -75,7 +75,18 @@ if (!file_exists($filename_print)) {
     if ($config['print']['qrcode'] && file_exists('../vendor/phpqrcode/lib/full/qrlib.php')) {
         // create qr code
         if (!file_exists($filename_codes)) {
-            if ($config['qr']['append_filename']) {
+            if ($config['ftp']['enabled'] && $config['ftp']['useForQr']) {
+                $destination = $config['ftp']['folder'];
+                if ($config['ftp']['appendDate']) {
+                    $destination .= DIRECTORY_SEPARATOR . date('Y/m/d');
+                }
+
+                if ($config['ftp']['website'] != '') {
+                    $url = $config['ftp']['website'] . $destination . DIRECTORY_SEPARATOR . $filename;
+                } else {
+                    $url = substr($destination . DIRECTORY_SEPARATOR . $filename, 1);
+                }
+            } elseif ($config['qr']['append_filename']) {
                 $url = $config['qr']['url'] . $filename;
             } else {
                 $url = $config['qr']['url'];
