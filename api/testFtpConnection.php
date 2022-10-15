0bd10cdc3ce442f9bbb03a1d3a98a788074f1fb9
Francesco <francesco.miccolis98@libero.it>
2022-10-15 15:07:34 +0200

    admin: added button to test the FTP connection

---
+++ api/testFtpConnection.php
@@ -0,0 +1,19 @@
+<?php
+header('Content-Type: application/json');
+
+require_once '../lib/log.php';
+
+$data = $_POST;
+
+// init connection to ftp server
+$ftp = ftp_ssl_connect($data['ftp[baseURL]'], $data['ftp[port]']);
+
+// login to ftp server
+$login_result = ftp_login($ftp, $data['ftp[username]'], $data['ftp[password]']);
+
+if (!$login_result) {
+    logError("Can't connect to FTP Server!");
+    die(json_encode("Can't connect to FTP Server!"));
+}
+
+die(json_encode("Connected to FTP Server!"));
--- lib/configsetup.inc.php
+++ lib/configsetup.inc.php
@@ -1848,6 +1848,13 @@ $configsetup = [
             'name' => 'ftp[password]',
             'value' => htmlentities($config['ftp']['password'] ?? ''),
         ],
+        'test_connection' => [
+            'view' => 'basic',
+            'type' => 'button',
+            'placeholder' => 'testConnection',
+            'name' => 'TESTCONNECTION',
+            'value' => 'test-connection',
+        ],
         'baseFolder' => [
             'view' => 'advanced',
             'type' => 'input',
