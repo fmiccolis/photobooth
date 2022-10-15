6a66566db1ca7fffd8f7283e9febfbcfb6f5d61f
Francesco <francesco.miccolis98@libero.it>
2022-10-15 19:00:15 +0200

    admin: adjusted variable names on testConnection

--- api/testFtpConnection.php
+++ api/testFtpConnection.php
@@ -6,14 +6,15 @@ require_once '../lib/log.php';
 $data = $_POST;
 
 // init connection to ftp server
-$ftp = ftp_ssl_connect($data['ftp[baseURL]'], $data['ftp[port]']);
+$ftp = ftp_ssl_connect($data['ftp']['baseURL'], $data['ftp']['port']);
 
 // login to ftp server
-$login_result = ftp_login($ftp, $data['ftp[username]'], $data['ftp[password]']);
+$login_result = ftp_login($ftp, $data['ftp']['username'], $data['ftp']['password']);
 
 if (!$login_result) {
     logError("Can't connect to FTP Server!");
     die(json_encode("Can't connect to FTP Server!"));
 }
+ftp_close($ftp);
 
 die(json_encode("Connected to FTP Server!"));
