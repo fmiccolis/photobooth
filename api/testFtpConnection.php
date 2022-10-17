cd7205af2706dec13b557cb69552cdb53f38e3c1
Francesco <francesco.miccolis98@libero.it>
2022-10-17 16:54:20 +0200

    config: fixed lang translation key

--- api/testFtpConnection.php
+++ api/testFtpConnection.php
@@ -14,7 +14,7 @@ $result = array(
 );
 
 foreach ($params as $param) {
-    if(!isset($data['ftp'][$param])) {
+    if($data['ftp'][$param] == '') {
         $result['missing'][] = $param;
     }
 }
--- lib/configsetup.inc.php
+++ lib/configsetup.inc.php
@@ -1851,7 +1851,7 @@ $configsetup = [
         'test_connection' => [
             'view' => 'basic',
             'type' => 'button',
-            'placeholder' => 'testConnection',
+            'placeholder' => 'test_connection',
             'name' => 'TESTCONNECTION',
             'value' => 'test-connection',
         ],
